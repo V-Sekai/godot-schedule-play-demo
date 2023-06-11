3a54a0a7caba8772aedf47d73d2e621948ce11b8
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-11 06:07:02 -0700

    WIP. Doesn't work.

--- schedule_play/Node2D.gd
+++ schedule_play/Node2D.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 func _ready():	
-	var mix_time = AudioServer.get_last_mix_time()
-	mix_time = int(mix_time)
-	for i in range(0, 1000):
-		var ap = get_node("AudioStreamPlayer").duplicate()
+	var mix_time: int = AudioServer.get_last_mix_time_usec()
+	for i in range(0, 5):
+		var ap: AudioStreamPlayer = get_node("AudioStreamPlayer").duplicate()
 		add_child(ap)
-		ap.play_scheduled(_generate_time(mix_time, i))
+		ap.owner = owner
+		ap.scheduled_time_usec = mix_time + _generate_time(mix_time, i)
+		ap.play()
 
-func _generate_time(mix_time, times):	
-	var interval_sec = .2
-	var next_audio_event = mix_time + interval_sec * times
+func _generate_time(mix_time, times) -> int:
+	var interval_usec: int = 2e6
+	var next_audio_event: int = interval_usec * times
 	print("Scheduling at mix time " + str(mix_time) + " at " + str(next_audio_event))
 	return next_audio_event
