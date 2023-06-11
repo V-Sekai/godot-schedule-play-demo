912c709e17c769ab6878b0d4d2ce2653999105b0
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-06-11 06:57:09 -0700

    Adjust code.

--- schedule_play/Node2D.gd
+++ schedule_play/Node2D.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
 func _ready():	
-	var mix_time: int = AudioServer.get_last_mix_time_usec()
+	var mix_time_usec: int = AudioServer.get_last_mix_time_usec() / 1000000
 	for i in range(0, 5):
 		var ap: AudioStreamPlayer = get_node("AudioStreamPlayer").duplicate()
 		add_child(ap)
 		ap.owner = owner
-		ap.scheduled_time_usec = mix_time + _generate_time(mix_time, i)
 		ap.play()
+		ap.scheduled_time = mix_time_usec + _generate_time(mix_time_usec, i)
 
-func _generate_time(mix_time, times) -> int:
-	var interval_usec: int = 2e6
-	var next_audio_event: int = interval_usec * times
-	print("Scheduling at mix time " + str(mix_time) + " at " + str(next_audio_event))
+func _generate_time(mix_time_usec, times) -> int:
+	var interval: float = 1
+	var next_audio_event: float = interval * times
+	print("Scheduling at mix time " + str(mix_time_usec) + " at " + str(next_audio_event))
 	return next_audio_event
